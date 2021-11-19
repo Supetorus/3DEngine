bc91d35cd7d03b2d566c223ca342fd6ba8c3c4fb
Supetorus <66396555+Supetorus@users.noreply.github.com>
2021-11-19 07:25:12 -0700

    ReadFileToString function

--- Engine/Core/FileSystem.cpp
+++ Engine/Core/FileSystem.cpp
@@ -1,5 +1,8 @@
 #include "FileSystem.h"
 #include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <SDL.h>
 
 namespace nc
 {
@@ -10,5 +13,22 @@ namespace nc
 	std::string GetFilePath()
 	{
 		return std::filesystem::current_path().string();
+	}
+
+	bool ReadFileToString(const std::string& filename, std::string& filestring)
+	{
+		std::ifstream fileStream(filename, std::ios::in);
+		if (!fileStream.is_open())
+		{
+			SDL_Log("Error: Failed to open file: %s", filename.c_str());
+			return false;
+		}
+
+		std::stringstream stream;
+		stream << fileStream.rdbuf();
+		filestring = stream.str();
+		fileStream.close();
+
+		return true;
 	}
 }
